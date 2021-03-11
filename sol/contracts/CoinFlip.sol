f8585cc8a5448b2d62c79212937e0bf98966191d
rube-de <rube@ruf2.de>
2021-03-11 23:55:02 +0100

    rework with structs

--- sol/contracts/CoinFlip.sol
+++ sol/contracts/CoinFlip.sol
@@ -6,42 +6,86 @@ import "./provableAPI.sol";
 contract CoinFlip is Ownable, usingProvable{
     uint public balance;
 
-    event coinFlipped(uint bettingAmount, address player);
+
+    mapping (bytes32 => address) private playerQuery;
+    mapping (bytes32 => uint) private randomResult;
+    //address[] private players;
+    mapping(address => Player) players;
+
+    uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
+    uint256 public latestNumber;
+
+    event coinFlipped(bytes32 queryId, bool won, uint bettingAmount, address player);
     event balancedRaised(uint value, address raiser);
     event payout(uint amount, address player);
     event wonFlip(uint amount, address player);
     event lostFlip(uint amount, address player);
-
     event changeBalance(uint balance);
+    event newCoinFlip(address player , bytes32 queryId, uint amount);
 
-    mapping (address => uint) private playerBalance;
-    address[] private players;
+    event LogNewProvableQuery(string msg);
+    event generatedRandomNumber(bytes32 indexed queryId, uint256 randomNumber);
 
-    function doCoinFlip() public payable returns (bool){
-        players.push(msg.sender);
-        uint result = random();
-        uint bettingAmount = msg.value;
+    struct Player {
+        address adr;
+        uint betAmount;
+        uint balance;
+        bool isPlaying;
+    }
+
+    struct Play {
+        address playerAddress;
+        bytes32 id;
+    }
+    constructor() public {
+      //  update();
+    }
+
+    function _createPlayer(address _adr, uint _betAmount) private returns(Player){
+        players[_adr] = Player(_adr,_betAmount, 0, false);
+        return players[_adr];
+    }
+
+    function doCoinFlip() public payable {
+        require (address(this).balance >= msg.value.mul(2), "Contract balance needs to be higher than winning amount");
+        Player currentPlayer;
+        if(players[msg.sender] != null){
+            require (players[msg.sender].isPlaying == false, "Currently in game");
+            currentPlayer = players[msg.sender];
+            currentPlayer.betAmount = msg.value;
+        } else{
+            currentPlayer = _createPlayer(msg.sender,msg.value);
+        }
+        currentPlayer.isPlaying = true;
+        bytes32 queryId = update();
+        playerQuery[queryId] = msg.sender;
         balance += msg.value;
         emit changeBalance(balance);
+        emit newCoinFlip(msg.sender, queryId, bettingAmount);
+    }
+
+    function calculateResult(bytes32 queryId) private {
+        uint result = randomResult[queryId];
+        Player player = players[playerQuery[queryId]];
         if(result == 1){
             //player won
-            require(balance >= bettingAmount *2, "Contract balance needs to be higher than winning amount");
-            playerBalance[msg.sender] = bettingAmount *2;
-            emit wonFlip(bettingAmount *2, msg.sender);
+            require(balance >= player.betAmount *2, "Contract balance needs to be higher than winning amount");
+            player.balance += player.betAmount *2;
+            emit coinFlipped(queryId, true, player.betAmount,player.adr);
+            emit wonFlip(bettingAmount, player);
         }else{
-            emit lostFlip(bettingAmount, msg.sender);
+            emit coinFlipped(queryId, false, player.betAmount,player.adr);
+            emit lostFlip(bettingAmount, player);
         }
-        emit coinFlipped(bettingAmount, msg.sender);
-        return result > 0;
     }
 
     function payoutPrize() public {
         //send to player adr
-        uint prizeAmount = playerBalance[msg.sender];
+        uint prizeAmount = players[msg.sender].balance;
         require(prizeAmount > 0);
         balance = balance - prizeAmount;
-        msg.sender.transfer(playerBalance[msg.sender]);
-        playerBalance[msg.sender] = 0;
+        msg.sender.transfer(prizeAmount);
+        players[msg.sender].balance = 0;
         emit payout(prizeAmount, msg.sender);
     }
 
@@ -67,5 +111,36 @@ contract CoinFlip is Ownable, usingProvable{
 
     function getPlayerBalance() public view returns(uint){
         return playerBalance[msg.sender];
+    }
+
+    function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+        //require(msg.sender == provable_cbAddress());
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % 2;
+        randomResult[_queryId] = randomNumber;
+        emit generatedRandomNumber(_queryId, randomNumber);
+        calculateResult(_queryId);
+    }
+
+    function update() payable public returns (bytes32){
+        uint256 QUERY_EXECUTION_DELAY = 0;
+        uint256 GAS_FOR_CALLBACK = 200000;
+      /*  bytes32 queryId = provable_newRandomDSQuery(
+        QUERY_EXECUTION_DELAY,
+        NUM_RANDOM_BYTES_REQUESTED,
+        GAS_FOR_CALLBACK
+        );*/
+        bytes32 queryId = testRandom();
+        emit LogNewProvableQuery("Provable query was sent, standing by for answer");
+        return queryId;
+    }
+
+    function testRandom() public returns(bytes32){
+        bytes32 queryId = bytes32(keccak256(abi.encodePacked(msg.sender)));
+        __callback(queryId, "1", bytes("test"));
+        return queryId;
+    }
+
+    function callCallback(bytes32 queryId) public {
+        __callback(queryId, "1", bytes("test"));
     }
 }
